9dbd1b99bc07e5e4c348e4752107929ead0725c1
sugimoto-ecn <sugimo.324@gmail.com>
2021-11-29 22:34:16 +0900

    refactor

--- parts/main.py
+++ parts/main.py
@@ -4,11 +4,18 @@ import requests
 from utils import time_format
 from models import btn
 import time
+import pigpio
+import json
 from LCD1602I2C.LCD import LCD
 from controller import lcd_clock_controller , active_buzzer_controller
 
+
+
+
 class Servo():
+    
     def __init__(self):
+        self.pi = pi = pigpio.pi()
         self.SERVO_MIN_PULSE = 500
         self.SERVO_MAX_PULSE = 2500
         self.pin = 18
@@ -18,27 +25,37 @@ class Servo():
 
     def setup(self):
         global p
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.pin, GPIO.OUT)
-        GPIO.output(self.pin, GPIO.LOW)
-        p = GPIO.PWM(self.pin, 50)     # set Frequecy to 50Hz
-        p.start(0)  
-    
-    def setAngle(self, angle):      # make the servo rotate to specific angle (0-180 degrees)
-        angle = max(0, min(180, angle))
-        pulse_width = self.map(angle, 0, 180, self.SERVO_MIN_PULSE, self.SERVO_MAX_PULSE)
-        pwm = self.map(pulse_width, 0, 20000, 0, 100)
-        p.ChangeDutyCycle(pwm)
+        # GPIO.setmode(GPIO.BCM)
+        # GPIO.setup(self.pin, GPIO.OUT)
+        # GPIO.output(self.pin, GPIO.LOW)
+        # p = GPIO.PWM(self.pin, 50)     # set Frequecy to 50Hz
+        # p.start(0) 
+        gp_out = 18
+        GPIO.setup(gp_out, GPIO.OUT) 
+        self.servo = GPIO.PWM(gp_out, 50)
+        self.servo.start(0)
+    # def setAngle(self, angle):      # make the servo rotate to specific angle (0-180 degrees)
+    #     angle = max(0, min(90, angle))
+    #     pulse_width = self.map(angle, 0, 90, self.SERVO_MIN_PULSE, self.SERVO_MAX_PULSE)
+    #     pwm = self.map(pulse_width, 0, 20000, 0, 100)
+    #     p.ChangeDutyCycle(pwm)
     
     def rock(self):
-        for i in range(0, 181, 5):   #make servo rotate from 0 to 180 deg
-            self.setAngle(i)     # Write to servo
-            time.sleep(0.002)
+        # for i in range(0, 91, 5):   #make servo rotate from 0 to 180 deg
+        #     self.setAngle(i)     # Write to servo
+        #     time.sleep(0.002)su
 
+        self.servo.ChangeDutyCycle(12)
+
+        # self.pi.set_servo_pulsewidth( SERVO_PIN, 1500 )
     def unrock(self):
-        for i in range(180, -1, -5): #make servo rotate from 180 to 0 deg
-            self.setAngle(i)
-            time.sleep(0.001)
+        # self.pi.set_servo_pulsewidth( SERVO_PIN, 500 )
+        self.servo.ChangeDutyCycle(7.25)
+        
+
+        # for i in range(90, -1, -5): #make servo rotate from 180 to 0 deg
+        #     self.setAngle(i)
+        #     time.sleep(0.001)
 
 class Keypad():
 
@@ -69,15 +86,17 @@ product_id = ['1', '1', '1', '1','1', '1', '1', '1']
 password = [0, 0, 0, 0]
 alerm_time = "**:**:**"
 BTN_PIN=21
+LED_PIN = 20
 clock = lcd_clock_controller.ClockController(alerm_time)
 button = btn.Btn(BTN_PIN)
 keypad = None
 last_key_pressed = None
 alerm=False
-url = 'https://api.thingspeak.com/update'
+url = 'http://35.74.101.128/v1'
 user_info = None
 SERVO_PIN=18
 servo = Servo()
+is_close = False
 
 
 
@@ -93,6 +112,10 @@ def set_alerm():
         alerm_time = response.json
         time.sleep(60*6)
 
+
+"""
+キーパッド関連
+"""
 def setup_key():
     global keypad, last_key_pressed, lcd
     rowsPins = [14,23,24,25]
@@ -108,14 +131,16 @@ def setup_key():
     lcd.message('WELCOME!',1)
     lcd.message('Enter id',2)
 
+
+"""
+モニター関連
+"""
 def show_display():
     global keypad, last_key_pressed
     while True:
         # lcd.clear()  
         current_time = time_format.current_time_format()
         pressed_keys = keypad.read()
-        # clock.show_current_time()
-        # print(button.status)
         if user_info == None and input_value_1[0] == "?":
             if button.status:
                 clock.show_current_time()
@@ -136,11 +161,31 @@ def show_display():
             lcd.message("".join(input_value_2), 2) 
         time.sleep(0.1)
 
+
+"""
+ユーザー情報取得＋格納
+"""
 def set_user():
-    global user_info, alerm_time, password
+    global user_info, alerm_time, password,url,input_value_1,servo
+    response = requests.post(url+'/users/product',json={
+        'id':"".join(input_value_1)
+    })
+    print('&&&&&&&&&&')
+    print(response.text)
+    datas = json.loads(response.text)
+    if "email" not in datas["user"]:
+        input_value_1[0]="?"
+        input_value_1[1]="?"
+        input_value_1[2]="?"
+        input_value_1[3]="?"
+        input_value_1[4]="?"
+        input_value_1[5]="?"
+        input_value_1[6]="?"
+        input_value_1[7]="?"
+        return
     user_info = {
-        "name":"user1",
-        "alerm":"00:04:30",
+        "name":datas["user"]["email"],
+        "alerm":datas["schedule"][0]["wakeup"],
         "password":"1111"
     }
     alerm_time = user_info["alerm"]
@@ -166,8 +211,12 @@ def check(target, value, LENS):
     return 1
 
 
+
+"""
+ブザーを鳴らす系
+"""
 def buzzer_func():
-    global button, servo, alerm
+    global button, servo, alerm,is_close
     BUZZER_PIN = 16
     active = active_buzzer_controller.ActiveBuzzerController(BUZZER_PIN)
 
@@ -194,24 +243,44 @@ def buzzer_func():
                     time.sleep(0.5)
                 if button.status:
                     alerm = False
+                    button.init_status()
+                    is_close = True
+
         time.sleep(0.1)
 
+
+
+def update_info():
+    global user_info
+    while True:
+        if user_info != None:
+            get_user()
+        time.sleep(6*60)
+
+    
+"""
+メインのループ
+"""
 def loop():
     global lcd
     global input_value_1, last_key_pressed,product_id
     global servo
-    global password,alerm
+    global password,alerm,is_close
     keyIndex=0
     LENS1 = 8
     LENS2 = 4
     setup_key()
+    
     display_thread = threading.Thread(target=show_display)
     display_thread.start()
 
     alerm_thread = threading.Thread(target=buzzer_func)
     alerm_thread.start()
+
+    user_info_thread = threading.Thread(target=update_info)
+    user_info_thread.start()
+
     servo.setup()
-    servo.unrock()
 
     while True:
         if user_info == None:
@@ -220,17 +289,8 @@ def loop():
                 input_value_1[keyIndex]=pressed_keys[0]
                 keyIndex+=1
             if (keyIndex is LENS1):
-                if (check(product_id, input_value_1,LENS1 ) is 1):
-                    set_user()
-                    servo.rock()
-                input_value_1[0]="?"
-                input_value_1[1]="?"
-                input_value_1[2]="?"
-                input_value_1[3]="?"
-                input_value_1[4]="?"
-                input_value_1[5]="?"
-                input_value_1[6]="?"
-                input_value_1[7]="?"
+                set_user()
+                servo.rock()
             keyIndex=keyIndex%LENS1
         else:
             pressed_keys = keypad.read()
@@ -241,11 +301,17 @@ def loop():
                 if (check(password, input_value_2,LENS2 ) is 1):
                     # set_user()
                     servo.unrock()
+                    is_close = True
                 input_value_2[0]="?"
                 input_value_2[1]="?"
                 input_value_2[2]="?"
                 input_value_2[3]="?"
             keyIndex=keyIndex%LENS2
+
+            if is_close == True:
+                if button.status:
+                    is_close = False
+                    servo.rock()
         last_key_pressed = pressed_keys
         time.sleep(0.1)
 
